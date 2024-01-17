0764e14fdf7608921da616a365490086def47f14
Võ Nguyễn Hoài Nam <vonguyenhoainam123@gmail.com>
2024-01-17 11:49:46 +0700

    Update camera.js

--- CLIENT/js/camera.js
+++ CLIENT/js/camera.js
@@ -86,7 +86,7 @@ document.addEventListener('DOMContentLoaded', () => {
         currentCamera = (currentCamera === 'user') ? 'environment' : 'user';
         startCamera();
         if (currentCamera === 'environment') {
-            cameraContainer.removeAttribute('transform');
+            cameraContainer.style.transform = 'scaleX(-1)';
         }
 
     });
